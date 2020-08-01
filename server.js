d6815f182b6f0a0f813a869fe3c084de5630a963
Kal <info@kalacademy.org>
2020-08-01 16:48:22 -0700

    ready for deployment

--- server.js
+++ server.js
@@ -1,44 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
-const passport = require('passport');
-const users = require('./routes/api/users');
-const posts = require('./routes/api/posts');
-const profile = require('./routes/api/profile');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
+const path = require("path");
 const app = express();
 
 //Body parser configuration
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 //Passport configuration
 app.use(passport.initialize());
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //Db config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //Connect to mongodb
 mongoose
   .connect(db)
-  .then(() => console.log('Mongodb connected'))
-  .catch(err => console.log(err));
+  .then(() => console.log("Mongodb connected"))
+  .catch((err) => console.log(err));
 
 //Let's write our first route
-app.get('/', (req, res)=> res.send('Hello'));
 
 //Use routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 //serve static assets if in production
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 8000;
