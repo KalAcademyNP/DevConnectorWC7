9881ac1dda80506c679fa8698c61950ca0afb0e6
Kal <info@kalacademy.org>
2020-08-02 14:38:22 -0700

    test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders developer connector', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/developer connector/i);
   expect(linkElement).toBeInTheDocument();
 });
